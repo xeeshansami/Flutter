ce3bc1209cb9c5c560950d8ca3b1dd55a5fb60c9
mac <info.xeeshan@gmail.com>
2023-01-12 21:38:14 +0500

    alerts on list view and its onClick

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -16,6 +16,7 @@ class FirstScreen extends StatelessWidget {
   Widget textVIew() {
     return Center(
         child: Container(
+            padding: const EdgeInsets.all(10.0),
             alignment: Alignment.center,
             child: Column(
               children: [
@@ -49,7 +50,41 @@ class FirstScreen extends StatelessWidget {
                                 fontFamily: "Ubuntu",
                                 fontWeight: FontWeight.w800)))
                   ],
-                )
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                            "Your lucky number is " + luckyNumber().toString(),
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.0,
+                                fontFamily: "Ubuntu",
+                                fontWeight: FontWeight.w800))),
+                    Expanded(
+                        child: Text(
+                            "Your lucky number is " + luckyNumber().toString(),
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.0,
+                                fontFamily: "Ubuntu",
+                                fontWeight: FontWeight.w800))),
+                    Expanded(
+                        child: Text(
+                            "Your lucky number is " + luckyNumber().toString(),
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.0,
+                                fontFamily: "Ubuntu",
+                                fontWeight: FontWeight.w800)))
+                  ],
+                ),
+                const imagePicker(),
+                const myButton(),
+
               ],
             )));
   }
@@ -58,5 +93,64 @@ class FirstScreen extends StatelessWidget {
     var random = Random();
     int luckyNumber = random.nextInt(10);
     return luckyNumber;
+  }
+
+  Widget getListView() {
+    var listView = ListView(
+      children: const [
+        ListTile(
+          leading: Icon(Icons.landscape),
+          title: Text("Landscape"),
+          subtitle: Text("This is my landscapes"),
+          trailing: Icon(Icons.sunny_snowing),
+        )
+      ],
+    );
+    return listView;
+  }
+}
+
+class imagePicker extends StatelessWidget {
+  const imagePicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = const AssetImage("assets/images/abc.jpg");
+    Image image = Image(image: assetImage, width: 100.0, height: 100.0);
+    return Container(child: image);
+  }
+}
+
+class myButton extends StatelessWidget {
+  const myButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+        backgroundColor: Colors.black12,
+        textStyle: const TextStyle(fontSize: 30.0));
+    const eds = EdgeInsets.all(10.0);
+    return Container(
+        width: 250.0,
+        height: 50.0,
+        padding: eds,
+        margin: eds,
+        child: ElevatedButton(
+          style: style,
+          onPressed: () => alertMe(context),
+          child: const Text("Click Me",
+              style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white,
+                  fontFamily: "Ubuntu",
+                  fontWeight: FontWeight.w800)),
+        ));
+  }
+
+  void alertMe(BuildContext context) {
+    var alertDialog = const AlertDialog(
+        title: Text("ALERT!"), content: Text("This is my alert dialog"));
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './app_screens/first_screen.dart';
@@ -18,6 +21,50 @@ class MyFlutterApp extends StatelessWidget {
                   style: TextStyle(color: Colors.white, fontSize: 30.0)),
               backgroundColor: Colors.red,
             ),
-            body: FirstScreen()));
+            body: getListView()));
+  }
+
+  List<String> getList() {
+    return List<String>.generate(1000, (index) => "Item $index");
+  }
+
+  Widget getListView() {
+    return ListView.builder(itemBuilder: (context,index){
+      return ListTile(
+          leading: const Icon(Icons.arrow_right),
+          title: Text("Item $index"),
+          subtitle: const Text("This is my landscapes"),
+          trailing: const Icon(Icons.sunny_snowing),
+          onTap: () => alertMe(context,"Item $index"));
+    });
+
+    var listView = ListView(
+      children: [
+        ListTile(
+            leading: Icon(Icons.landscape),
+            title: Text("Landscape"),
+            subtitle: Text("This is my landscapes"),
+            trailing: Icon(Icons.sunny_snowing),
+            onTap: () => debugPrint("click me")),
+        ListTile(
+            leading: Icon(Icons.landscape),
+            title: Text("Landscape"),
+            subtitle: Text("This is my landscapes"),
+            trailing: Icon(Icons.sunny_snowing),
+            onTap: () => debugPrint("Click his"))
+      ],
+    );
+    return listView;
+  }
+  int luckyNumber() {
+    var random = Random();
+    int luckyNumber = random.nextInt(10);
+    return luckyNumber;
+  }
+  void alertMe(BuildContext context, String text) {
+    var alertDialog =  AlertDialog(
+        title:  Text(text), content: const Text("assayed"));
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
   }
 }
